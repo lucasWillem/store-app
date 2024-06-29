81be34cdf6e1ea5377fde19216705e8af26e1693
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-06-29 12:07:30 +0200

    [feature][add_routing]: Add Login, Unauthorized and Store pages.

---
+++ src/pages/UnauthorizedPage.template.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import { FC } from "react";
+
+const UnauthorizedPage: FC = () => {
+  return <Typography variant="body1">Unauthorized</Typography>;
+};
+
+export default UnauthorizedPage;
