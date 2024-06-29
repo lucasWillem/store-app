4379116b10437953c88ea6d7bab482be37530964
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-06-30 00:33:36 +0200

    [feature][state_management]:  configure easy-peasy to store cart items in shopping cart

--- src/redux/models.ts
+++ src/redux/models.ts
@@ -3,7 +3,9 @@ import { persist } from "easy-peasy";
 import type { StoreModel } from "./types";
 
 import productsModel from "./products-model";
+import shoppingCartModel from "./shoppingcart-model";
 
 export const models: StoreModel = {
   products: persist(productsModel),
+  cartItems: persist(shoppingCartModel),
 };
---
+++ src/redux/shoppingcart-model.ts
@@ -0,0 +1,26 @@
+import { Action, action } from "easy-peasy";
+
+interface CartItem {
+  id: number;
+  title: string;
+  price: string;
+  category: string;
+  description: string;
+  image: string;
+}
+
+//TODO: co-locate models with their respective components
+
+export interface ShoppingCartModel {
+  cartItems: CartItem[];
+  addCartItem: Action<ShoppingCartModel, CartItem>;
+}
+
+const shoppingCartModel: ShoppingCartModel = {
+  cartItems: [],
+  addCartItem: action((state, payload) => {
+    state.cartItems.push(payload);
+  }),
+};
+
+export default shoppingCartModel;
