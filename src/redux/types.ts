d4084937cd531bc7d74374ae059ca5cbe270449e
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-06-30 00:33:36 +0200

    [feature][state_management]:  configure easy-peasy to store cart items in shopping cart

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,5 +1,7 @@
 import type { ProductsModel } from "./products-model";
+import type { ShoppingCartModel } from "./shoppingcart-model";
 
 export interface StoreModel {
   products: ProductsModel;
+  cartItems: ShoppingCartModel;
 }
