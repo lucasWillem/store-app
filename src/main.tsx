92a00ae24e114326f1ff9499c0e784b01f7404b2
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-06-29 12:21:32 +0200

    [format][add_routing]: remove file extensions from imports in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "@mui/material";
-import { theme } from "./global/theme/theme.ts";
+import { theme } from "./global/theme/theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
