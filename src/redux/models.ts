15ccfc2380737d4d891f2528f33a63dd32a36032
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-07-02 15:04:22 +0200

    [feature][user_authentication]: add user-management organism, and add user-model to the store

--- src/redux/models.ts
+++ src/redux/models.ts
@@ -2,10 +2,12 @@ import { persist } from "easy-peasy";
 
 import { type StoreModel } from "./types";
 
+import userModel from "@/components/organisms/user-management/state/user-model";
 import productsModel from "@/components/organisms/store-management/state/products-model";
 import shoppingCartModel from "@/components/organisms/shopping-cart-management/state/shoppingcart-model";
 
 export const models: StoreModel = {
   products: persist(productsModel),
   cartItems: persist(shoppingCartModel),
+  user: persist(userModel),
 };
--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,6 +1,8 @@
+import { type UserModel } from "@/components/organisms/user-management/state/user-model";
 import { type ProductsModel } from "@/components/organisms/store-management/state/products-model";
 import { type ShoppingCartModel } from "@/components/organisms/shopping-cart-management/state/shoppingcart-model";
 export interface StoreModel {
   products: ProductsModel;
   cartItems: ShoppingCartModel;
+  user: UserModel;
 }
