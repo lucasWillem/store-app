86618e9d06855a64915426ef89b21b23aa66986b
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-07-02 18:55:41 +0200

    [chore][user_authentication]: wrap AppProvider around App in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { StoreProvider } from "easy-peasy";
-import { ThemeProvider } from "@mui/material";
-import { theme } from "@/styles/theme";
-import { store } from "@/redux";
-import { AppWrapper } from "@/components/templates/AppWrapper";
-import { NetworkProvider } from "@/networking";
+import { AppProvider } from "@/components/templates/AppProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <StoreProvider store={store}>
-        <NetworkProvider>
-          <AppWrapper>
-            <App />
-          </AppWrapper>
-        </NetworkProvider>
-      </StoreProvider>
-    </ThemeProvider>
+    <AppProvider>
+      <App data-cy="app" />
+    </AppProvider>
   </React.StrictMode>,
 );
