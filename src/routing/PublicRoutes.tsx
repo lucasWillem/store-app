7ba77753f89e9407758cd80297ccf09749090b8d
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-07-02 16:04:28 +0200

    [chore][user_authentication]:  use the RoutePaths enum for paths in Private and Public routes.

--- src/routing/PublicRoutes.tsx
+++ src/routing/PublicRoutes.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import { Outlet, Navigate } from "react-router-dom";
+import { RoutePaths } from "@/global";
 
 interface PublicRoutesProps {
   isAuthenticated: boolean;
 }
 
 const PublicRoutes: FC<PublicRoutesProps> = ({ isAuthenticated }) => {
-  return isAuthenticated ? <Navigate to="/store" /> : <Outlet />;
+  return isAuthenticated ? <Navigate to={RoutePaths.Store} /> : <Outlet />;
 };
 
 export { PublicRoutes };
