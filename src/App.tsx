b7e715aad39daca025d3cb2e44258cae6b951e2e
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-07-02 16:28:25 +0200

    [feature][user_authentication]:  implement logout feature, by injecting a positioned AuthMenu into the Router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import { Router } from "@/routing";
+import { AuthMenu } from "@/components/organisms/user-management/containers/AuthMenu";
+import { useTheme } from "@mui/material";
 
 function App() {
-  return <Router />;
+  const theme = useTheme();
+
+  return (
+    <Router>
+      <AuthMenu
+        containerStyle={{
+          position: "absolute",
+          top: theme.spacing(2),
+          left: theme.spacing(2),
+        }}
+      />
+    </Router>
+  );
 }
 
 export default App;
