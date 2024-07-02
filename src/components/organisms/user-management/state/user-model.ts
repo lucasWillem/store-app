8c11ea56c3de46f7159e4ec7776817c979dbecf3
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-07-02 15:04:22 +0200

    [feature][user_authentication]: add user-management organism, and add user-model to the store

---
+++ src/components/organisms/user-management/state/user-model.ts
@@ -0,0 +1,24 @@
+import { Action, action } from "easy-peasy";
+
+interface User {
+  username: string;
+  jwt: string;
+}
+
+export interface UserModel {
+  user?: User;
+  storeUser: Action<UserModel, User>;
+  logout: Action<UserModel>;
+}
+
+const userModel: UserModel = {
+  user: undefined,
+  storeUser: action((state, payload) => {
+    state.user = payload;
+  }),
+  logout: action((state) => {
+    state.user = undefined;
+  }),
+};
+
+export default userModel;
