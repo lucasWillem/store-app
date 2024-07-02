5825a4a8018733edf1fa63eba9a4629f2804823a
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-07-02 13:52:19 +0200

    [feature][user_authentication]: wrap StoreProvider around App

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { StoreProvider } from "easy-peasy";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./global/theme/theme";
+import { store } from "@/redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <StoreProvider store={store}>
+        <App />
+      </StoreProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
