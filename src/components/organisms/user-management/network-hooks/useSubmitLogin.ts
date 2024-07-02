1cb0bd0ff059b48dfb835d1a44a6a904fe2fa600
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-07-02 15:24:08 +0200

    [feature][user_authentication]: add Login user functionality to the user-management organism, including the LoginForm.container, useSubmitLogin network-hook, and storing the successfully logged in user in redux

---
+++ src/components/organisms/user-management/network-hooks/useSubmitLogin.ts
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+
+import {
+  type CustomStrapiError,
+  type StrapiServerResponse,
+  handleNetworkError,
+  type CustomMutationOptions,
+} from "@/networking";
+
+type Data = {
+  identifier: string;
+  password: string;
+};
+
+type Response = StrapiServerResponse<{
+  jwt?: string;
+  user?: {
+    id: number;
+    username: string;
+    email: string;
+    createdAt: Date;
+    updatedAt: Date;
+  } | null;
+}>;
+
+export const mutationKey = ["SubmitLogin"];
+
+async function submitLogin(url: string, data: Data): Promise<Response> {
+  try {
+    const response = await axios.post<Response>(url, data);
+    return response.data;
+  } catch (error) {
+    return handleNetworkError(error as CustomStrapiError);
+  }
+}
+
+export const useSubmitLogin = ({
+  url,
+  options,
+}: {
+  url: string;
+  options?: CustomMutationOptions<Response, Data>;
+}) => {
+  const mutationOptions = options ?? {};
+
+  return useMutation({
+    mutationKey,
+    mutationFn: (data: Data) => submitLogin(url, data),
+    ...mutationOptions,
+  });
+};
