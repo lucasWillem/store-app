87e5428887c9c66f1c52f3227a24a47de0eddc1d
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-07-02 15:31:23 +0200

    [feature][user_authentication]: Add StorePage. Instantiate the LoginForm container in the LoginPage, and instantiate the SignUpForm container in the SignUpPage.

--- src/pages/LoginPage.template.ts
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-
-const LoginPage: FC = () => {
-  return null;
-};
-
-export default LoginPage;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
-export { default as StorePage } from "./LoginPage.template";
+export { default as StorePage } from "./StorePage.template";
 export { default as UnauthorizedPage } from "./UnauthorizedPage.template";
 export { default as LoginPage } from "./LoginPage.template";
+export { default as SignUpPage } from "./SignUpPage.template";
