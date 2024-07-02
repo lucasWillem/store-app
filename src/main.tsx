2c416ccd15add38839e3c3e313714d8123090ddc
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-07-02 14:40:04 +0200

    [feature][user_authentication]: Add and implement AppWrapper template, replacing index.css and App.css, and move theme.ts into styles/theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import { StoreProvider } from "easy-peasy";
 import { ThemeProvider } from "@mui/material";
-import { theme } from "./global/theme/theme";
+import { theme } from "@/styles/theme";
 import { store } from "@/redux";
+import { AppWrapper } from "@/components/templates/AppWrapper";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <StoreProvider store={store}>
-        <App />
+        <AppWrapper>
+          <App />
+        </AppWrapper>
       </StoreProvider>
     </ThemeProvider>
   </React.StrictMode>,
