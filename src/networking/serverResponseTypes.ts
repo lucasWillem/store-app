72fce5093f95361dc3b4ea008e6023e387cf9e18
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-07-02 22:26:56 +0200

    [task][add_loader_and_alert_presentational_components]:  update types, for CustomStrapiError and authentication network-hooks responses.

--- src/networking/serverResponseTypes.ts
+++ src/networking/serverResponseTypes.ts
@@ -1,12 +1,16 @@
 import { type AxiosError } from "axios";
 
-export type CustomStrapiError = AxiosError<StrapiServerResponse["error"]>;
+export type CustomStrapiError = AxiosError<
+  StrapiServerResponse["errorResponse"]
+>;
 
 export type StrapiServerResponse<SuccessResponseBody = unknown> =
   SuccessResponseBody & {
-    error?: {
-      status: number;
-      name: string;
-      message: string;
+    errorResponse?: {
+      error?: {
+        status: number;
+        message: string;
+        name: string;
+      };
     };
   };
