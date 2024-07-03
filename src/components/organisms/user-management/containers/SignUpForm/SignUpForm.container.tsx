680dcb37f55cda576cb20e005b2b67b4d50c594c
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-07-03 02:25:33 +0200

    [task][add_loader_and_alert_presentational_components]:  Implement Loader for user interaction in LoginForm, SignUpForm and Router

--- src/components/organisms/user-management/containers/SignUpForm/SignUpForm.container.tsx
+++ src/components/organisms/user-management/containers/SignUpForm/SignUpForm.container.tsx
@@ -43,29 +43,39 @@ const SignUpForm: FC = () => {
     (actions) => actions.alert.configureAlert,
   );
 
+  const configureLoader = useStoreActions(
+    (actions) => actions.loader.configureLoader,
+  );
+
   const navigate = useNavigate();
 
   const { mutate: registerUser } = useSignUpUser({
     url: AuthenticationEndPoints.Register,
     options: {
-      onError: (error) => {
-        const { message } = error as Error;
-        configureAlert({
-          isVisible: true,
-          message: message,
-          severity: AlertSeverity.Error,
-        });
+      onMutate: () => {
+        configureLoader({ isVisible: true });
       },
-      onSuccess: (userData) => {
-        const { jwt, user } = userData;
+      onSettled: (data, error) => {
+        configureLoader({ isVisible: false });
 
-        if (!jwt || !user) return;
+        if (error) {
+          const { message } = error as Error;
+          configureAlert({
+            isVisible: true,
+            message: message,
+            severity: AlertSeverity.Error,
+          });
+        } else if (data) {
+          const { jwt, user } = data;
 
-        storeUser({
-          jwt,
-          username: user.username,
-        });
-        navigate(RoutePaths.Store);
+          if (!jwt || !user) return;
+
+          storeUser({
+            jwt,
+            username: user.username,
+          });
+          navigate(RoutePaths.Store);
+        }
       },
     },
   });
