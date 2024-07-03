a513a92818cb77530fcbe8311f31f6a1a7c28c2f
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-07-03 15:21:31 +0200

    [feature][add_and_implement_page_wrapper_template]: add PageWrapper.template for generic (yet customizable) styling of pages

---
+++ src/components/templates/PageWrapper/PageWrapper.styles.ts
@@ -0,0 +1,15 @@
+import { Box, styled } from "@mui/material";
+
+const StyledPageWrapper = styled(Box)(({ theme }) => ({
+  background: theme.palette.background.default,
+  position: "absolute",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
+export { StyledPageWrapper };
