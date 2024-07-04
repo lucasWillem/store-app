8ec31b9a00e7130654c1290e90addeb6bd0cecd0
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-07-04 21:28:05 +0200

    [feature][shopping_cart_implementation]: address responsiveness issues, affecting the rendering of shopping cart icon.

--- src/components/utilities/FlatList/FlatList.styles.ts
+++ src/components/utilities/FlatList/FlatList.styles.ts
@@ -1,7 +1,7 @@
 import { styled, Box, Theme } from "@mui/material";
 
 const StyledFlatList = styled(Box)<{ theme?: Theme }>(({ theme }) => ({
-  maxWidth: "90vw",
+  maxWidth: "85vw",
   maxHeight: "90vh",
   display: "grid",
   gridTemplateColumns: "repeat(auto-fill, minmax(240px, 1fr))",
@@ -13,6 +13,21 @@ const StyledFlatList = styled(Box)<{ theme?: Theme }>(({ theme }) => ({
   },
   msOverflowStyle: "none",
   scrollbarWidth: "none",
+  [theme.breakpoints.down(1000)]: {
+    maxWidth: "80vw",
+  },
+  [theme.breakpoints.down(750)]: {
+    maxWidth: "75vw",
+  },
+  [theme.breakpoints.down(650)]: {
+    maxWidth: "70vw",
+  },
+  [theme.breakpoints.down(500)]: {
+    maxWidth: "65vw",
+  },
+  [theme.breakpoints.down(380)]: {
+    maxWidth: "62vw",
+  },
 }));
 
 export { StyledFlatList };
